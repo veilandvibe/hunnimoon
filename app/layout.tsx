7787f06166e921f9c85fa751a08a8776c098e4fe
Matthew Le <matthewle@Matthews-MBP-2.lan>
2026-02-02 21:38:41 -0800

    Update Meta Pixel ID to correct tracking code
    Changes pixel ID to 4411798279143546.

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,7 +78,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '702502992712129');
+              fbq('init', '4411798279143546');
               fbq('track', 'PageView');
             `
           }}
@@ -88,7 +88,7 @@ export default function RootLayout({
             height="1" 
             width="1" 
             style={{display: 'none'}}
-            src="https://www.facebook.com/tr?id=702502992712129&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=4411798279143546&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
